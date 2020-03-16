d8a436a3bc9955fa6fb2eaa9e043adcd5cd63ca2
@Deepak Pal <paldeepaksofttest@gmail.com>
2020-03-16 17:03:15 +0530

    "16/03/2020"

--- MoshCoding/expressdemo/index1.js
+++ MoshCoding/expressdemo/index1.js
@@ -1,6 +1,14 @@
 const Joi = require('joi');
 const express = require('express');
+const helmet = require('helmet');
+const morgan= require('morgan');
 const app = express();
+const config = require('config');
+
+//to set the env for the modules
+//process.env.NODE_ENV 
+// console.log(`NODE_ENV:${process.env.NODE_ENV}`); //not set any thing that is why unknown
+// console.log(`app:${app.get('env')}`); //by default return development env
 
 app.use(express.json());
 
@@ -18,6 +26,23 @@ app.use(express.urlencoded({extended:true}));  //key=value&&key=value
 app.use(express.static('public')); //thing inside the public folder can be used directly
 //now check localhost:3000/readmeFile.txt
 
+app.use(helmet());
+
+
+if(app.get('env')==='development'){
+    app.use(morgan('tiny')); //logger module to make logger pretty
+    console.log('Morgan  enabled....')
+}
+//using config file
+//configuration
+console.log('Application Name:'+config.get('name'));
+console.log('Mail Server:'+config.get('mail.host'));
+console.log('Mail Password:'+config.get('mail.password'));
+
+//it genrate logger for all your req ,so assume that in production you dont require to see the 
+//log details of the application , also it will pe maintain in api pipeline so 
+//optimization will be less, so we need to enable only in one env that can be set using the env
+//above has set the env variable
 
 const courses = [
     { id: 1, name: 'Action' },  
